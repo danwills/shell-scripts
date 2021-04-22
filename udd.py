2fa7b19a0522a6b3274e33e10a39f8fa174d2be4
Dan Wills <gdanzo@gmail.com>
2021-04-22 20:57:53 +0930

    Comment and alias added

--- udd.py
+++ udd.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-
+# Despite it being a bit horrible because of needing a temp-file, this is the most
+# reliable way I've found to make this work, make sure this file is in the path 
+# and then alias it like so:
+# alias udd="udd.py > ~/tmp/udd ; source ~/tmp/udd"
+# There must be another way (in zsh? for sure!)
 import os
 
 cwd = os.getcwd()
