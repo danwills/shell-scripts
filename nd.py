bb989985d270c394167ae19831dea06b3926da8b
Dan Wills <gdanzo@gmail.com>
2021-04-22 20:39:55 +0930

    Bit of explanatory comments, and an example zsh alias

--- nd.py
+++ nd.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+# Support for an alias that can make and enter directories, 
+# including with spaces in the name and multiple levels at once if there are slashes '/'.
+# This can be aliased in zsh (bash too I hope, maybe with some translation?) as follows:
+# Make sure this file is in the path, then:
+#function ndir
+#{
+#	cd "`nd.py \"$@\"`"
+#}
+#alias nd=ndir
 
 import sys
 import os
